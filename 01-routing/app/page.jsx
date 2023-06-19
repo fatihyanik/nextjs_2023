7e282beef968b5a8b99175b035f028842fb477b9
fatihyanik <fatihyanik07@gmail.com>
2023-06-19 23:37:58 +0200

    finish routing example

--- 01-routing/app/page.jsx
+++ 01-routing/app/page.jsx
@@ -4,13 +4,27 @@ export default function Home() {
   return (
     <div>
       <Link href="/hakkimizda">Hakkimizda</Link>
-      <br/>
+      <br />
       <Link href="/docs/post-1">Post 1</Link>
-      <br/>
+      <br />
       <Link href="/docs/post-1/post-2">Post 2</Link>
-      <br/>
+      <br />
       <Link href="/docs/post-1">Post 1</Link>
-     
+      <br />
+      <Link
+        target="_blank"
+        className="test"
+        href={{
+          pathname: "/hakkimizda",
+          query: {
+            name: "Next.js",
+            surname: "ff"
+          }
+        }}
+      >
+        Hakkimizda obje ile
+      </Link>
+
     </div>
   )
 }
